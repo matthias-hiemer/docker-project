eaa134748ab0d86a8057d3834681cc536d427c87
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2024-10-21 10:54:03 +0300

    OAuth

---
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration // @Service more descriptive to what this bean does
+@EnableWebSecurity //  Enables Spring Security
+public class SecurityConfig {
+
+    @Value("${app.url}")
+    private String appUrl; // We redirect to this URL on successful login
+
+    @Bean
+    // This bean is what configures all aspects of Spring Security
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(a -> a
+                .requestMatchers("/api/**").authenticated() // This covers /api/hello, /api/hello/1, everything with a /api prefix
+                .anyRequest().permitAll()
+            )
+            .csrf(AbstractHttpConfigurer::disable) // This protection is only applicable for server side rendered apps
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)) // Store logged in user in HTTP session
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))) // If user fails to login return 401 unauthorized status
+            .oauth2Login(o -> o.defaultSuccessUrl(appUrl)); // Redirect to appUrl if login is successful
+        return http.build();
+    }
+
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [username, setUsername] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+        window.open(host + '/oauth2/authorization/github', '_self')
+        // http://localhost:8080/oauth2/authorization/github
+    }
+
+    const loadUser = () => {
+        axios.get('/api/auth/me')
+            .then(response => {
+                const usernameResponse = response.data;
+                console.log(usernameResponse)
+                setUsername(usernameResponse)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    useEffect(() => {
+        loadUser()
+    }, []);
+
+    return (
+        <>
+            <button onClick={login}>
+                Login with GitHub
+            </button>
+
+            {username &&
+                <h1>Welcome, {username}</h1>
+            }
+        </>
+    )
 }
 
 export default App
