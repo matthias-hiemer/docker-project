dd31769ebf66bf7905590af1557b3af567c0e260
matthias <matthias.hiemer@scitlab.de>
2024-10-21 17:02:00 +0200

    Add a dummy settings page to demonstrate routes

---
+++ frontend/src/Settings.tsx
@@ -0,0 +1,13 @@
+
+
+function Settings() {
+    return (
+        <div>
+            <h1>My Settings</h1>
+
+            <img src={'/profile.png'} width="200" alt="profile"/>
+        </div>
+    );
+}
+
+export default Settings;
