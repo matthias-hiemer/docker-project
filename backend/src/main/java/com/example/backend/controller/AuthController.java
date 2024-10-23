2732e290542d681d560dc4434ebd327c64bf1fb1
matthias <matthias.hiemer@scitlab.de>
2024-10-23 09:12:45 +0200

    Refactor /me endpoint to return app user object

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.AppUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -14,9 +17,13 @@ public class AuthController {
 
     // GET /api/auth/me
     @GetMapping("/me")
-    public String getMe(@AuthenticationPrincipal OAuth2User user) {
-        log.info("User: {}", user);
-        log.info("User.attributes: {}", user.getAttributes());
-        return user.getAttributes().get("login").toString(); // This is the user's username e.g. MahmoudFawzyKhalil
+    public AppUser getMe(@AuthenticationPrincipal OAuth2User user) {
+
+        return AppUser.builder()
+                .id(user.getName())
+                .username(user.getAttribute("login"))
+                .avatarUrl(user.getAttribute("avatar_url"))
+                .favorites(List.of())
+                .build();
     }
 }
---
+++ backend/src/main/java/com/example/backend/model/AppUser.java
@@ -0,0 +1,14 @@
+package com.example.backend.model;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record AppUser(
+        String id,
+        String username,
+        String avatarUrl,
+        List<String> favorites
+) {
+}
